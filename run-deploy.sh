48f74de505fe0cdf7b92b5ee3c14def3de268575
sandreas <2050604+sandreas@users.noreply.github.com>
2023-12-22 20:50:58 +0100

    removed useless index.php and updated deployment script

--- run-deploy.sh
+++ run-deploy.sh
@@ -2,8 +2,12 @@ SYNC_DIR="public"
 FTP_HOST="$(grep 'host' .credentials | cut -d '=' -f 2)"
 FTP_USERNAME="$(grep 'username' .credentials | cut -d '=' -f 2)"
 FTP_OBSCURED_PASSWORD="$(grep 'password' .credentials | cut -d '=' -f 2)"
-rclone sync --update "$SYNC_DIR" ":ftp:/$SYNC_DIR/" \
+TOKEN_SECRET="$(grep 'token_secret' .credentials | cut -d '=' -f 2)"
+
+sed -i "s/<use-a-strong-token-secret-here>/${TOKEN_SECRET}/g" "public/index.php" \
+  && rclone sync --update "$SYNC_DIR" ":ftp:/$SYNC_DIR/" \
     --ftp-concurrency=10 \
     --ftp-host="$FTP_HOST" \
     --ftp-user="$FTP_USERNAME" \
-    --ftp-pass="$FTP_OBSCURED_PASSWORD"
+    --ftp-pass="$FTP_OBSCURED_PASSWORD" \
+  && sed -i "s/${TOKEN_SECRET}/<use-a-strong-token-secret-here>/g" "public/index.php"
